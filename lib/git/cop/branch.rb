c3c46bcda162770cd7cdc8bc222557fb9ac0f617
Ana María Martínez Gómez <ammartinez@suse.de>
2017-07-04 09:44:53 +0200

    Added Travis support.
    We need to consider the case of a PR and a branch separately.
    
    The main problem is that Travis only "load" the local branch, so it
    doesn't know things likes origin/master. Also in a PR, Travis call the
    branch master so we need to find the correct branch.
    
    There is only one case when this doesn't work and is if you run git-cop
    rspec test on Travis. But as Circle CI is used this shouldn't be a
    problem.
    
    Fixes https://github.com/bkuhlmann/git-cop/issues/12

--- lib/git/cop/branch.rb
+++ lib/git/cop/branch.rb
@@ -10,10 +10,16 @@ module Git
       end
 
       def name
+        if ENV["TRAVIS"] == "true"
+          pr_branch = ENV["TRAVIS_PULL_REQUEST_BRANCH"]
+          return pr_branch unless pr_branch.empty?
+          return ENV["TRAVIS_BRANCH"]
+        end
         "#{path}#{`git rev-parse --abbrev-ref HEAD | tr -d '\n'`}"
       end
 
       def shas
+        set_travis if ENV["TRAVIS"] == "true"
         `git log --pretty=format:"%H" #{master}..#{name}`.split("\n")
       end
 
@@ -23,6 +29,18 @@ module Git
 
       def master
         "#{path}master"
+      end
+
+      def set_travis
+        pr_slug = ENV["TRAVIS_PULL_REQUEST_SLUG"]
+        unless pr_slug.empty?
+          # origin is set to the upstream in a PR in Travis
+          `git remote add original_branch https://github.com/#{pr_slug}.git`
+          `git fetch original_branch #{name}:#{name}`
+        end
+        # Travis doesn't know master by default
+        `git remote set-branches --add origin master`
+        `git fetch`
       end
     end
   end
